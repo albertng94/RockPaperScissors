e4e3a622b864517e71082be475bb2403f2716814
albertng94 <albertng94@gmail.com>
2023-05-20 13:51:56 +0200

    Create randomSymbol variable. Modify randomNumber.
    randomNumber is modified by removing the ".tostring" transformation.
    thus, randomNumber outputs a random integer from 1 to 3.
    
    randomSymbol is created with an empty value. Then, an if .. else
    structure is used to input a value of "rock", "paper" or "scissors"
    to randomSymbol depending on randomNumber's current value.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 
-//Function getRandomInt returns a random integer between the two numerical parameters (included)
-//Variable randomInt executes getRandomInt function to generate a random number from 1 to 3 and converts the result to the string type.
+//Function getRandomInt returns a random integer between the two numerical parameters (included).
+//Variable randomInt executes getRandomInt function to generate a random number from 1 to 3.
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -9,5 +9,19 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); 
 }
 
-let randomInt = getRandomIntInclusive(1, 3).toString();
-console.log(randomInt);
+let randomNumber = getRandomInt(1, 3);
+console.log(randomNumber);
+
+//Variable randomSymbol is created without a value. 
+//The value given to randomSymbol will be either "rock", "paper" or "scissors" based on randomNumber's current value. An if .. else structure is used to that end.
+let randomSymbol;
+
+if (randomNumber === 1) {
+    randomSymbol = "rock";
+} else if (randomNumber === 2) {
+    randomSymbol = "paper";
+} else if (randomNumber === 3) {
+    randomSymbol = "scissors";
+}
+
+console.log(randomSymbol);
