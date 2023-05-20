5b1c66ca559cf0855250236c1c9e590e442b5ccd
albertng94 <albertng94@gmail.com>
2023-05-20 12:45:58 +0200

    Create function getRandomInt and variable randomNumber.
    Function getRandomInt generates a random integer between two
    specified integers included. Variable randomNumber executes
    getRandomInt to generate a random integer between 1 and 3 and
    converts the resulting integer to the string type.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+
+
+//Function getRandomInt returns a random integer between the two numerical parameters (included)
+//Variable randomInt executes getRandomInt function to generate a random number from 1 to 3 and converts the result to the string type.
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+let randomInt = getRandomIntInclusive(1, 3).toString();
+console.log(randomInt);
